6fae77b8548ef6fe1fd797f8543d14d380e4a8ac
shruti-linux <131751004+shruti-linux@users.noreply.github.com>
2025-03-31 13:17:51 +0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,7 +4,6 @@ provider "azurerm" {
 subscription_id = "853976da-1943-482f-be67-2d729c61ef50"
 }
 
-resource "azurerm_resource_group" "example2" {
-  name     = "rg1-shruti666"
-  location = "westus"
+output "hello_world" {
+  value = "Hello, World!"
 }
